46f2f9f902c022bf6dedd84ca74768cc2cab3fe9
jayant812 <50166500+jayant812@users.noreply.github.com>
2021-12-13 11:59:15 +0530

    EHI Contact Information
    Added new project EHI Contact Information

---
+++ Controllers/EmployeeContactInfoController.cs
@@ -0,0 +1,46 @@
+﻿using EHI.Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EHI.Controllers
+{
+    public class EmployeeContactInfoController : ApiController
+    {
+       public Irepository<EmployeeContactInfoModel> objRepository;
+        public EmployeeContactInfoController()
+        {
+            objRepository = new Repository<EmployeeContactInfoModel>();
+        }
+        // GET: api/EmployeeContactInfo
+        public IEnumerable<EmployeeContactInfoModel> Get()
+        {
+            return objRepository.GetData();
+        }
+
+        // GET: api/EmployeeContactInfo/5
+        public EmployeeContactInfoModel GetDataById(int id)
+        {
+            return objRepository.GetDataById(id);
+        }
+
+        // POST: api/EmployeeContactInfo
+        public void Post([FromBody] EmployeeContactInfoModel Request)
+        {
+        }
+
+        // PUT: api/EmployeeContactInfo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/EmployeeContactInfo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Repository/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHI.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Get all data
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetData();
+
+        /// <summary>
+        /// Fetch Data by its id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        T GetDataById(long Id);
+
+        /// <summary>
+        /// Insert data into DB
+        /// </summary>
+        /// <param name="data"></param>
+        void InsertData(T data);
+
+        /// <summary>
+        /// Deete specific data from DB
+        /// </summary>
+        /// <param name="Id"></param>
+        void DeleteData(int Id);
+
+        /// <summary>
+        /// Update specific record
+        /// </summary>
+        /// <param name="data"></param>
+        void UpdateData(T data);
+
+        /// <summary>
+        /// Save data 
+        /// </summary>
+        void Save();
+    }
+}
