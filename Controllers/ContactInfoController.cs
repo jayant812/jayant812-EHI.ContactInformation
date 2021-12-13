e9a6e9a6fb1e1ff3b37c883e769fc99f0c55bd64
jayant812 <50166500+jayant812@users.noreply.github.com>
2021-12-13 11:59:15 +0530

    EHI Contact Information
    Added new project EHI Contact Information

---
+++ Controllers/ContactInfoController.cs
@@ -0,0 +1,76 @@
+﻿
+using EHI.Models;
+using EHI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace EHI.Controllers
+{
+    //to enable CORS should add be attribute 
+    //[EnableCors(origins: "http://localhost.com/test", headers: "*", methods: "*")]
+    public class ContactInfoController : ApiController
+    {
+        public IRepository<EmpContactInfo> objRepository;
+        public ContactInfoController()
+        {
+            objRepository = new Repository<EmpContactInfo>();
+        }
+
+
+        [Route("~/EHI/GetContactInformation")]
+        public IEnumerable<EmpContactInfo> Get()
+        {
+            return objRepository.GetData();
+        }
+
+        [Route("~/EHI/GetContactInformationById")]
+        public EmpContactInfo GetDataById(long id)
+        {
+            return objRepository.GetDataById(id);
+        }
+
+        [Route("~/EHI/InsertContactInformation")]
+        public IHttpActionResult Post([FromBody] EmpContactInfo Request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            objRepository.InsertData(Request);
+            objRepository.Save();
+            return Ok(this.Get().OrderByDescending(x => x.Id));
+        }
+
+        [Route("~/EHI/UpdateContactInformation")]
+        public IHttpActionResult Put(long id,   EmpContactInfo Request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != Request.Id)
+            {
+                return BadRequest();
+            }
+
+            objRepository.UpdateData(Request);
+            objRepository.Save();
+
+            return Ok(this.GetDataById(id));
+        }
+
+        [Route("~/EHI/DeleteContactInformation")]
+        public IHttpActionResult Delete(int id)
+        {
+            objRepository.DeleteData(id);
+            objRepository.Save();
+
+            return Ok(this.Get());
+        }
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EHI.Models;
+namespace EHI.Repository
+{
+    //Implement Generic repository 
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private EHI_Contact_InfoEntities1 ehiDbContext;
+        private IDbSet<T> dbEntities;
+
+        //Constructor to create db object & set db entity of particular class
+        public Repository()
+        {
+            ehiDbContext = new EHI_Contact_InfoEntities1();
+
+            //Use to set db entity of particular class which we pass 
+            dbEntities = ehiDbContext.Set<T>();
+        }
+
+
+        /// <summary>
+        /// Deete specific data from DB
+        /// </summary>
+        /// <param name="Id"></param>
+        public void DeleteData(int Id)
+        {
+            T data = dbEntities.Find(Id);
+            dbEntities.Remove(data);
+        }
+
+        /// <summary>
+        /// Get all data
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> GetData()
+        {
+            return dbEntities.ToList();
+        }
+
+        /// <summary>
+        /// Fetch Data by its id
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public T GetDataById(long Id)
+        {
+            return dbEntities.Find(Id);
+        }
+
+        /// <summary>
+        /// Insert data into DB
+        /// </summary>
+        /// <param name="data"></param>
+        public void InsertData(T data)
+        {
+            dbEntities.Add(data);
+        }
+
+        /// <summary>
+        /// Save data 
+        /// </summary>
+        public void Save()
+        {
+            ehiDbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Update specific record
+        /// </summary>
+        /// <param name="data"></param>
+        public void UpdateData(T data)
+        {
+            ehiDbContext.Entry(data).State = EntityState.Modified;
+        }
+    }
+}
