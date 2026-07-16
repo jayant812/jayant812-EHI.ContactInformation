7e499fe2c87eb420698bd4830ec27658ba32166f
jayant812 <50166500+jayant812@users.noreply.github.com>
2026-07-16 17:03:38 +0530

    Add password variable to Get method

--- Controllers/ContactInfoController.cs
+++ Controllers/ContactInfoController.cs
@@ -25,6 +25,7 @@ namespace EHI.Controllers
         [Route("~/EHI/GetContactInformation")]
         public IEnumerable<EmpContactInfo> Get()
         {
+        var password=" ";
             return objRepository.GetData();
         }
 
